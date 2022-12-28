e1d9fbc1c25d866f418041a363d965aa0f482d06
jjfreeburn <118950347+jjfreeburn@users.noreply.github.com>
2022-12-27 23:19:43 -0800

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,6 +1,13 @@
 const findSum = function(array) {
   // your code here - don't forget to return a number!
 };
+const number = [2, 4, 6];
+
+const findSum = number.reduce(function (a, b) {
+  return a + b;
+}, 0);
+
+console.log(findSum);
 
 const findFrequency = function(array) {
   // your code here - don't forget to return an object!
