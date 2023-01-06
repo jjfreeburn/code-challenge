faab712c89bf4055787f37ae900b8487de06f2a4
jjfreeburn <118950347+jjfreeburn@users.noreply.github.com>
2023-01-06 14:31:14 -0800

    Update submission.js

--- submission.js
+++ submission.js
@@ -22,32 +22,16 @@ const isPalindrome = function(str) {
 };
 
 const largestPair = function(array) {
-  // your code here - don't forget to return a number!
-  const array = [1, 3, 5, 8];
-
-const largestPair = function (array) {
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    const elementAjacent = array[i + 1];
-    const product = element * elementAjacent;
-    const result = Math.max(...products);
-    console.log(result);
-  }
-};
-
-largestPair(array);
-};
-const array = [9, 5, 10, 2, 24, 10, 148];
-function adjacentElementsProduct(array) {
   let maxProduct = array[0] * array[1];
   for (let i = 1; i < array.length; i++) {
     product = array[i] * array[i + 1];
 
     if (product > maxProduct) maxProduct = product;
   }
+
   return maxProduct;
 }
-console.log(adjacentElementsProduct(array));
+
 
 
 const removeParenth = function(str) {
