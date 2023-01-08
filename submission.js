87f5c6cb6e8dfebf4d5a6cc1ec468803c79d1ddf
jjfreeburn <118950347+jjfreeburn@users.noreply.github.com>
2023-01-08 15:51:01 -0800

    Update submission.js

--- submission.js
+++ submission.js
@@ -68,5 +68,38 @@ const removeparenth = function (str) {
 
 
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+  let alphabetScore = {
+    a: 1,
+    e: 1,
+    i: 1,
+    o: 1,
+    u: 1,
+    l: 1,
+    n: 1,
+    r: 1,
+    s: 1,
+    t: 1,
+    d: 2,
+    g: 2,
+    b: 3,
+    c: 3,
+    m: 3,
+    p: 3,
+    f: 4,
+    h: 4,
+    v: 4,
+    w: 4,
+    y: 4,
+    k: 5,
+    j: 8,
+    x: 8,
+    q: 10,
+    z: 10,
+  };
+  let sum = 0;
+  for (i = 0; i < str.length; i++) {
+    sum += alphabetScore[str[i]];
+  }
+  return sum;
+};
 };
